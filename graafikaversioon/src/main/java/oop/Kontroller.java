30ae837ad8eac0081626a15cae5e53bc0313d0dd
eliskaar <eliisemai@gmail.com>
2020-05-03 14:54:50 +0300

    graafikaviimistlused

--- graafikaversioon/src/main/java/oop/Kontroller.java
+++ graafikaversioon/src/main/java/oop/Kontroller.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -18,6 +19,17 @@ public class Kontroller {
     private Pane liitmine;
 
     private int tehteTüüp;
+
+    @FXML
+    private StackPane liitminenupp;
+    @FXML
+    private StackPane lahutamine;
+    @FXML
+    private StackPane korrutamine;
+    @FXML
+    private StackPane jagamine;
+    @FXML
+    private StackPane kombineeritud;
 
     File fail = new File("oigedvastused.txt");
 
@@ -34,6 +46,7 @@ public class Kontroller {
         lava.showAndWait();
 
     }
+
     @FXML
     public void lahutamineVajuta(MouseEvent avt) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("raskus.fxml"));
@@ -46,6 +59,7 @@ public class Kontroller {
         lava.setScene(new Scene(juur));
         lava.showAndWait();
     }
+
     @FXML
     public void korrutamineVajuta(MouseEvent avt) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("raskus.fxml"));
@@ -58,6 +72,7 @@ public class Kontroller {
         lava.setScene(new Scene(juur));
         lava.showAndWait();
     }
+
     @FXML
     public void jagamineVajuta(MouseEvent avt) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("raskus.fxml"));
@@ -70,6 +85,7 @@ public class Kontroller {
         lava.setScene(new Scene(juur));
         lava.showAndWait();
     }
+
     @FXML
     public void kombineeritudVajuta(MouseEvent avt) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("raskus.fxml"));
@@ -94,6 +110,17 @@ public class Kontroller {
         Stage p = (Stage) nupp.getScene().getWindow();
         p.close();
         System.exit(0);
+    }
+
+    @FXML
+    public void enter(MouseEvent avt){
+        Pane p = (Pane) avt.getSource();
+        p.setOpacity(0.0);
+    }
+    @FXML
+    public void exit(MouseEvent avt){
+        Pane p = (Pane) avt.getSource();
+        p.setOpacity(100.0);
     }
 
 
